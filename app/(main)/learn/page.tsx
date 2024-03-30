d9c268901c6661abbd0efc6fb40b4078bf720cd5
michel-reyes <mreyesh85@gmail.com>
2024-03-30 19:35:26 -0400

    add main learn layout add sticky sidebar add main feed and sticky header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,23 @@
+import { FeedWrapper } from '@/components/feed-wrapper';
+import { StickyWrapper } from '@/components/sticky-wrapper';
+import { Header } from './header';
+import { UserProgress } from '@/components/user-progress';
+
 export default function Learn() {
   return (
-    <div>
-      <p className="text-green-500 font-bold">Learn lingo</p>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: 'sample', imageSrc: '/es.svg' }}
+          hearts={3}
+          points={12}
+          hasActiveSubscription={true}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="sample" />
+        sample
+      </FeedWrapper>
     </div>
   );
 }
