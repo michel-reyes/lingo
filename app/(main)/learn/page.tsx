858bcfeb01e73195d059077c518d72a41603e110
michel-reyes <mreyesh85@gmail.com>
2024-04-04 00:06:50 -0400

    add seed for db add flag and title based on course selection

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,21 +2,30 @@ import { FeedWrapper } from '@/components/feed-wrapper';
 import { StickyWrapper } from '@/components/sticky-wrapper';
 import { Header } from './header';
 import { UserProgress } from '@/components/user-progress';
+import { getUserProgress } from '@/db/queries';
+import { redirect } from 'next/navigation';
 
-export default function Learn() {
+export default async function LearnPage() {
+  const UserProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([UserProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses');
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: 'sample', imageSrc: '/es.svg' }}
-          hearts={3}
-          points={12}
-          hasActiveSubscription={true}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="sample" />
-        sample
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
---
+++ db/queries.ts
@@ -0,0 +1,46 @@
+import { cache } from 'react';
+import db from '@/db/drizzle';
+import { eq } from 'drizzle-orm';
+import { auth } from '@clerk/nextjs';
+import { userProgress, courses } from '@/db/schema';
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
+
+export const getCourses = cache(async () => {
+  const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    // with: {
+    //   units: {
+    //     orderBy: (units, { asc }) => [asc(units.order)],
+    //     with: {
+    //       lessons: {
+    //         orderBy: (lessons, { asc }) => [asc(lessons.order)],
+    //       },
+    //     },
+    //   },
+    // },
+  });
+
+  return data;
+});
