dc3b902a5e5db501ff9c96f6437680bf6239e579
michel-reyes <mreyesh85@gmail.com>
2024-04-19 17:54:45 -0400

    add final page add footer

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { challenges, challengeOptions } from '@/db/schema';
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { Header } from './header';
 import { QuestionBubble } from './question-bubble';
 import { Challenge } from './challenge';
 import { Footer } from './footer';
+import Image from 'next/image';
+import Confetti from 'react-confetti';
+import { upsertChallengeProgress } from '@/actions/challenge-progress';
+import { toast } from 'sonner';
+import { reduceHearts } from '@/actions/user-progress';
+import { useAudio, useWindowSize } from 'react-use';
+import { ResultCard } from './result-card';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   initialPercentage: number;
@@ -24,8 +32,16 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
-  const [hearts, setHearts] = useState(50 || initialHearts);
-  const [percentage, setPercentage] = useState(45 || initialPercentage);
+  const [finishAudio] = useAudio({ src: '/finish.mp3', autoPlay: true });
+  const [correctAudio, _c, correctControls] = useAudio({ src: '/correct.wav' });
+  const [incorrectAudio, _i, incorrectControls] = useAudio({
+    src: '/incorrect.wav',
+  });
+  const [lessonId] = useState(initialLessonId);
+  const [pending, startTransition] = useTransition();
+
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
   const [challenges] = useState(initialLessonChallenges);
   const [activeIndex, setActiveIndex] = useState(() => {
     const uncompletedIndex = challenges.findIndex(
@@ -36,14 +52,126 @@ export const Quiz = ({
 
   const [selectedOption, setSelectedOption] = useState<number | undefined>(0);
   const [status, setStatus] = useState<'correct' | 'wrong' | 'none'>('none');
+  const router = useRouter();
+  const { width, height } = useWindowSize();
 
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onNext = () => {
+    setActiveIndex((current) => current + 1);
+  };
 
   const onSelect = (id: number) => {
     if (status !== 'none') return;
     setSelectedOption(id);
   };
+
+  const onContinue = () => {
+    if (!selectedOption) return;
+
+    if (status === 'wrong') {
+      setStatus('none');
+      setSelectedOption(undefined);
+      return;
+    }
+
+    if (status === 'correct') {
+      onNext();
+      setStatus('none');
+      setSelectedOption(undefined);
+      return;
+    }
+
+    const correctOption = options.find((option) => option.correct);
+
+    if (!correctOption) {
+      return;
+    }
+
+    if (correctOption.id === selectedOption) {
+      startTransition(() => {
+        upsertChallengeProgress(challenge.id)
+          .then((response) => {
+            if (response?.error === 'hearts') {
+              // openHeartsModal();
+              return;
+            }
+
+            correctControls.play();
+            setStatus('correct');
+            setPercentage((prev) => prev + 100 / challenges.length);
+
+            // This is a practice
+            if (initialPercentage === 100) {
+              setHearts((prev) => Math.min(prev + 1, 5));
+            }
+          })
+          .catch(() => toast.error('Something went wrong. Please try again.'));
+      });
+    } else {
+      startTransition(() => {
+        reduceHearts(challenge.id)
+          .then((response) => {
+            if (response?.error === 'hearts') {
+              // openHeartsModal();
+              return;
+            }
+
+            incorrectControls.play();
+            setStatus('wrong');
+
+            if (!response?.error) {
+              setHearts((prev) => Math.max(prev - 1, 0));
+            }
+          })
+          .catch(() => toast.error('Something went wrong. Please try again.'));
+      });
+    }
+  };
+
+  if (!challenge) {
+    return (
+      <>
+        {finishAudio}
+        <Confetti
+          width={width}
+          height={height}
+          recycle={false}
+          numberOfPieces={500}
+          tweenDuration={10000}
+        />
+        <div className="flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+          <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="hidden lg:block"
+            height={100}
+            width={100}
+          />
+          <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="block lg:hidden"
+            height={50}
+            width={50}
+          />
+          <h1 className="text-xl lg:text-3xl font-bold text-neutral-700">
+            Great job! <br /> You&apos;ve completed the lesson.
+          </h1>
+          <div className="flex items-center gap-x-4 w-full">
+            <ResultCard variant="points" value={challenges.length * 10} />
+            <ResultCard variant="hearts" value={hearts} />
+          </div>
+        </div>
+        <Footer
+          lessonId={lessonId}
+          status="completed"
+          onCheck={() => router.push('/learn')}
+        />
+      </>
+    );
+  }
 
   const title =
     challenge.type === 'ASSIST'
@@ -51,6 +179,8 @@ export const Quiz = ({
       : challenge.question;
   return (
     <>
+      {incorrectAudio}
+      {correctAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
@@ -71,14 +201,18 @@ export const Quiz = ({
                 onSelect={onSelect}
                 status={status}
                 selectedOption={selectedOption}
-                disabled={false}
+                disabled={pending}
                 type={challenge.type}
               />
             </div>
           </div>
         </div>
       </div>
-      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
+      <Footer
+        disabled={pending || !selectedOption}
+        status={status}
+        onCheck={onContinue}
+      />
     </>
   );
 };
